11b4efdf105d54fa3af767f8d96ca3d76de8f8b1
Maciej Rzadzinski <maciej.rzd@gmail.com>
2022-11-04 13:25:00 +0100

    Style header

---
+++ src/modules/templateHTML.js
@@ -0,0 +1,11 @@
+import gitIcon from '../img/github.png';
+
+function createTemplateHTML() {
+    const body = document.querySelector('body');
+    body.innerHTML += ' ';
+    const footerImg = document.querySelector('#github');
+    footerImg.src = gitIcon;
+}
+
+export default createTemplateHTML;
+
